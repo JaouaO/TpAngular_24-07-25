64f8e997adf471ec59d50cf35ec9cc25e1b42d34
JaouaO <jaoua.o@gmail.com>
2025-07-24 10:31:01 +0200

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import {MovieListComponent} from './pages/movie-list/movie-list.component';
+
+export const routes: Routes = [
+  {path:'movies', component: MovieListComponent},
+
+];
